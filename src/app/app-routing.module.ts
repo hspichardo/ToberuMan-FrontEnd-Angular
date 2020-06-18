08fffd4ff6373a75f3863d50a043cb7bf0b64d07
Steven <hspichardo@gmail.com>
2020-06-18 23:01:40 +0200

    adding table component #27

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ import {OverviewComponent} from './home/overview/overview.component';
 import {OrderComponent} from './home/order/order.component';
 import {InvoiceComponent} from './home/invoice/invoice.component';
 import {CousineComponent} from './home/cousine/cousine.component';
+import {TableComponent} from './home/table/table.component';
 
 
 const routes: Routes = [
@@ -19,7 +20,8 @@ const routes: Routes = [
       {path: 'overview', component: OverviewComponent},
       {path: 'order', component: OrderComponent},
       {path: 'invoice', component: InvoiceComponent},
-      {path: 'cousine', component: CousineComponent}
+      {path: 'cousine', component: CousineComponent},
+      {path: 'table', component: TableComponent}
     ]
   }
 ];
